bc9a579f58a5abff20cb12bdd655529184ee45ee
Aaron <aaron.hyu@gmail.com>
2026-03-06 16:27:27 -0800

    Add lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
+import { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './index.css';
-import { Landing } from './pages/Landing';
-import { Home } from './pages/Home';
-import { Database } from './pages/Database';
-import { Optimizer } from './pages/Optimizer';
+
+const Landing = lazy(() => import('./pages/Landing').then((module) => ({ default: module.Landing })));
+const Home = lazy(() => import('./pages/Home').then((module) => ({ default: module.Home })));
+const Database = lazy(() => import('./pages/Database').then((module) => ({ default: module.Database })));
+const Optimizer = lazy(() => import('./pages/Optimizer').then((module) => ({ default: module.Optimizer })));
 
 function App() {
     return (
-        <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/characters" element={<Home />} />
-            <Route path="/database" element={<Database />} />
-            <Route path="/doll/:dollName" element={<Optimizer />} />
-        </Routes>
+        <Suspense fallback={<div style={{ display: 'flex', height: '100vh', width: '100vw', justifyContent: 'center', alignItems: 'center' }}>Loading...</div>}>
+            <Routes>
+                <Route path="/" element={<Landing />} />
+                <Route path="/characters" element={<Home />} />
+                <Route path="/database" element={<Database />} />
+                <Route path="/doll/:dollName" element={<Optimizer />} />
+            </Routes>
+        </Suspense>
     );
 }
 
